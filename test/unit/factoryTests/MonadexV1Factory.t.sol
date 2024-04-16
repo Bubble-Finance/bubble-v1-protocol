fb4fbba8fb77a9fd7441f23409c6d92dce229277
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-04-16 20:43:36 +0530

    test: Set up tests

---
+++ test/unit/factoryTests/MonadexV1Factory.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import { FactoryHelper } from "../../helpers/FactoryHelper.sol";
+import { UniversalHelper } from "../../helpers/UniversalHelper.sol";
+
+contract MonadexV1FactoryTests is FactoryHelper, UniversalHelper { }
