ea133221e4a1ac23aeedf50d4181769232019cc1
Ola Hamid <abdulhamidjuwon@gmail.com>
2024-04-28 04:30:56 +0100

    latest update 4/28/2024

--- src/auxiliary/airdrop/MonadexV1AirdropManager.sol
+++ src/auxiliary/airdrop/MonadexV1AirdropManager.sol
@@ -31,19 +31,21 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {MerkleProof} from "../../../lib/openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
 //NOTE: Limitation add non reentrancy
 
 contract MonadexV1AirdropManager is Ownable, ReentrancyGuard {
     using Address for address;
     using SafeERC20 for IERC20;
+
     /////////////////////
     ///state variables///
     /////////////////////
-
+    bytes32 public merkleRoot;
     address[] public eligibleAddresses;
     address[] private s_Tokens;
     mapping(address => bool isSupported) public m_supportedToken;
-    mapping(address => bool eligible) public m_eligibleUser;
+    // mapping(address => bool eligible) public m_eligibleUser;
     mapping (address => bool ) public m_hasClaimed;
     uint256 public maxAddressLimit;
     uint256 public claimAmount;
@@ -54,7 +56,8 @@ contract MonadexV1AirdropManager is Ownable, ReentrancyGuard {
     error Monadex_UnsupportedAirdropToken(address token);
     error Monadex_ZeroAddressError();
     error Monadex_maxAddressLimit(uint256 maxAddressLimit, uint256 receiverLength);
-    error Monadex_IneligibleAddressError();
+    // error Monadex_IneligibleAddressError();
+    error Monadex_InvalidMekleproofError();
     error Monadex_newAddressAlreadyExisted();
     error Monadex_HasClaimedError();
 
@@ -63,13 +66,14 @@ contract MonadexV1AirdropManager is Ownable, ReentrancyGuard {
     //////////
     event E_TokenToClaim(address token, address claimer);
     event E_directTokenToclaim(address token, uint256 amount);
-    event E_addNewUserAddress(address newAddr);
+    // event E_addNewUserAddress(address newAddr);
     event E_addAirdropfund(address token, uint256 amountToAdd);
     event E_addToken(address token);
 
-    constructor(uint256 _claimAmountPerWallet, uint256 _maxAddressLimit) Ownable(msg.sender) {
+    constructor(uint256 _claimAmountPerWallet, uint256 _maxAddressLimit, bytes32 _merkleRoot) Ownable(msg.sender) {
         claimAmount = _claimAmountPerWallet;
         maxAddressLimit = _maxAddressLimit;
+        merkleRoot = _merkleRoot;
     }
 
     function addToken(address newToken) external onlyOwner {
@@ -125,13 +129,18 @@ contract MonadexV1AirdropManager is Ownable, ReentrancyGuard {
         emit E_directTokenToclaim(supportedToken, amount);
     }
     //limitation users can claim more than once 
-    function claimAirdrop(address supportedToken) external nonReentrant {
+    function claimAirdrop(address supportedToken, bytes32[] memory proof) external nonReentrant {
+
+        bytes32 leaf = keccak256(abi.encode(msg.sender));
+        if (!MerkleProof.verify(proof, merkleRoot, leaf)) {
+            revert Monadex_InvalidMekleproofError();
+        }
         if (m_supportedToken[supportedToken] != true) {
             revert Monadex_UnsupportedAirdropToken(supportedToken);
         }
-        if (m_eligibleUser[msg.sender] != true) {
-            revert Monadex_IneligibleAddressError();
-        }
+        // if (m_eligibleUser[msg.sender] != true) {
+        //     revert Monadex_IneligibleAddressError();
+        // }
         if (m_hasClaimed[msg.sender] == true) {
             revert Monadex_HasClaimedError();
         }
@@ -142,18 +151,18 @@ contract MonadexV1AirdropManager is Ownable, ReentrancyGuard {
         emit E_TokenToClaim(supportedToken, msg.sender);
     }
 
-    function addEligibleAddress(address newAddress) public onlyOwner {
-        if (m_eligibleUser[newAddress]) {
-            revert Monadex_newAddressAlreadyExisted();
-        }
-        if (newAddress == address(0)) {
-            revert Monadex_ZeroAddressError();
-        }
-        m_eligibleUser[newAddress] = true;
-        eligibleAddresses.push(newAddress);
+    // function addEligibleAddress(address newAddress) public onlyOwner {
+    //     if (m_eligibleUser[newAddress]) {
+    //         revert Monadex_newAddressAlreadyExisted();
+    //     }
+    //     if (newAddress == address(0)) {
+    //         revert Monadex_ZeroAddressError();
+    //     }
+    //     m_eligibleUser[newAddress] = true;
+    //     eligibleAddresses.push(newAddress);
 
-        emit E_addNewUserAddress(newAddress);
-    }
+    //     emit E_addNewUserAddress(newAddress);
+    // }
 
     /////////////////////
     ///getter function///
@@ -162,7 +171,4 @@ contract MonadexV1AirdropManager is Ownable, ReentrancyGuard {
         return s_Tokens[TokenID];
     }
 
-    function getEligibleAddresses(uint256 eligibleAddressID) public view returns (address) {
-        return eligibleAddresses[eligibleAddressID];
-    }
 }
